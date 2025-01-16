df4d067bd8914cda98ecae751a4409c7652afbe5
RAGAV <121377164+RAGAV203@users.noreply.github.com>
2025-01-16 22:55:52 +0530

    Update utils.py

--- runners/utils.py
+++ runners/utils.py
@@ -5,7 +5,266 @@ from PIL import Image
 from datetime import datetime
 from torchvision.utils import make_grid, save_image
 from Register import Registers
-from datasets.custom import CustomSingleDataset, CustomAlignedDataset, CustomInpaintingDataset
+import random
+from pathlib import Path
+
+from torch.utils.data import Dataset
+import torchvision.transforms as transforms
+
+from datasets.utils import get_image_paths_from_dir
+import cv2
+
+def get_image_paths_from_dir(fdir):
+    flist = os.listdir(fdir)
+    flist.sort()
+    image_paths = []
+    for i in range(0, len(flist)):
+        fpath = os.path.join(fdir, flist[i])
+        if os.path.isdir(fpath):
+            image_paths.extend(get_image_paths_from_dir(fpath))
+        else:
+            image_paths.append(fpath)
+    return image_paths
+
+class ImagePathDataset(Dataset):
+    def __init__(self, image_paths, image_size=(256, 256), flip=False, to_normal=False):
+        self.image_size = image_size
+        self.image_paths = image_paths
+        self._length = len(image_paths)
+        self.flip = flip
+        self.to_normal = to_normal # 是否归一化到[-1, 1]
+
+    def __len__(self):
+        if self.flip:
+            return self._length * 2
+        return self._length
+
+    def __getitem__(self, index):
+        p = 0.0
+        if index >= self._length:
+            index = index - self._length
+            p = 1.0
+
+        transform = transforms.Compose([
+            transforms.RandomHorizontalFlip(p=p),
+            transforms.Resize(self.image_size),
+            transforms.ToTensor()
+        ])
+
+        img_path = self.image_paths[index]
+        image = None
+        try:
+            image = Image.open(img_path)
+        except BaseException as e:
+            print(img_path)
+
+        if not image.mode == 'RGB':
+            image = image.convert('RGB')
+
+        image = transform(image)
+
+        if self.to_normal:
+            image = (image - 0.5) * 2.
+            image.clamp_(-1., 1.)
+
+        image_name = Path(img_path).stem
+        return image, image_name
+
+
+
+@Registers.datasets.register_with_name('custom_single')
+class CustomSingleDataset(Dataset):
+    def __init__(self, dataset_config, stage='train'):
+        super().__init__()
+        self.image_size = (dataset_config.image_size, dataset_config.image_size)
+        image_paths = get_image_paths_from_dir(os.path.join(dataset_config.dataset_path, stage))
+        self.flip = dataset_config.flip if stage == 'train' else False
+        self.to_normal = dataset_config.to_normal
+
+        self.imgs = ImagePathDataset(image_paths, self.image_size, flip=self.flip, to_normal=self.to_normal)
+
+    def __len__(self):
+        return len(self.imgs)
+
+    def __getitem__(self, i):
+        return self.imgs[i], self.imgs[i]
+
+
+@Registers.datasets.register_with_name('custom_aligned')
+class CustomAlignedDataset(Dataset):
+    def __init__(self, dataset_config, stage='train'):
+        super().__init__()
+        self.image_size = (dataset_config.image_size, dataset_config.image_size)
+        image_paths_ori = get_image_paths_from_dir(os.path.join(dataset_config.dataset_path, f'{stage}/B'))
+        image_paths_cond = get_image_paths_from_dir(os.path.join(dataset_config.dataset_path, f'{stage}/A'))
+        self.flip = dataset_config.flip if stage == 'train' else False
+        self.to_normal = dataset_config.to_normal
+
+        self.imgs_ori = ImagePathDataset(image_paths_ori, self.image_size, flip=self.flip, to_normal=self.to_normal)
+        self.imgs_cond = ImagePathDataset(image_paths_cond, self.image_size, flip=self.flip, to_normal=self.to_normal)
+
+    def __len__(self):
+        return len(self.imgs_ori)
+
+    def __getitem__(self, i):
+        return self.imgs_ori[i], self.imgs_cond[i]
+
+
+@Registers.datasets.register_with_name('custom_colorization_LAB')
+class CustomColorizationLABDataset(Dataset):
+    def __init__(self, dataset_config, stage='train'):
+        super().__init__()
+        self.image_size = (dataset_config.image_size, dataset_config.image_size)
+        self.image_paths = get_image_paths_from_dir(os.path.join(dataset_config.dataset_path, stage))
+        self.flip = dataset_config.flip if stage == 'train' else False
+        self.to_normal = dataset_config.to_normal
+        self._length = len(self.image_paths)
+
+    def __len__(self):
+        if self.flip:
+            return self._length * 2
+        return self._length
+
+    def __getitem__(self, index):
+        p = False
+        if index >= self._length:
+            index = index - self._length
+            p = True
+
+        img_path = self.image_paths[index]
+        image = None
+        try:
+            image = cv2.imread(img_path)
+            if self.to_lab:
+                image = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
+        except BaseException as e:
+            print(img_path)
+
+        if p:
+            image = cv2.flip(image, 1)
+        image = cv2.resize(image, self.image_size, interpolation=cv2.INTER_LINEAR)
+        image = torch.Tensor(image)
+        image = image.permute(2, 0, 1).contiguous()
+
+        if self.to_normal:
+            image = (image - 127.5) / 127.5
+            image.clamp_(-1., 1.)
+
+        L = image[0:1, :, :]
+        ab = image[1:, :, :]
+        cond = torch.cat((L, L, L), dim=0)
+        return image, cond
+
+
+@Registers.datasets.register_with_name('custom_colorization_RGB')
+class CustomColorizationRGBDataset(Dataset):
+    def __init__(self, dataset_config, stage='train'):
+        super().__init__()
+        self.image_size = (dataset_config.image_size, dataset_config.image_size)
+        self.image_paths = get_image_paths_from_dir(os.path.join(dataset_config.dataset_path, stage))
+        self.flip = dataset_config.flip if stage == 'train' else False
+        self.to_normal = dataset_config.to_normal
+        self._length = len(self.image_paths)
+
+    def __len__(self):
+        if self.flip:
+            return self._length * 2
+        return self._length
+
+    def __getitem__(self, index):
+        p = False
+        if index >= self._length:
+            index = index - self._length
+            p = True
+
+        transform = transforms.Compose([
+            transforms.RandomHorizontalFlip(p=p),
+            transforms.Resize(self.image_size),
+            transforms.ToTensor()
+        ])
+
+        img_path = self.image_paths[index]
+        image = None
+        try:
+            image = Image.open(img_path)
+        except BaseException as e:
+            print(img_path)
+
+        if not image.mode == 'RGB':
+            image = image.convert('RGB')
+
+        cond_image = image.convert('L')
+        cond_image = cond_image.convert('RGB')
+
+        image = transform(image)
+        cond_image = transform(cond_image)
+
+        if self.to_normal:
+            image = (image - 0.5) * 2.
+            image.clamp_(-1., 1.)
+            cond_image = (cond_image - 0.5) * 2.
+            cond_image.clamp_(-1., 1.)
+
+        image_name = Path(img_path).stem
+        return (image, image_name), (cond_image, image_name)
+
+
+@Registers.datasets.register_with_name('custom_inpainting')
+class CustomInpaintingDataset(Dataset):
+    def __init__(self, dataset_config, stage='train'):
+        super().__init__()
+        self.image_size = (dataset_config.image_size, dataset_config.image_size)
+        self.image_paths = get_image_paths_from_dir(os.path.join(dataset_config.dataset_path, stage))
+        self.flip = dataset_config.flip if stage == 'train' else False
+        self.to_normal = dataset_config.to_normal
+        self._length = len(self.image_paths)
+
+    def __len__(self):
+        if self.flip:
+            return self._length * 2
+        return self._length
+
+    def __getitem__(self, index):
+        p = 0.
+        if index >= self._length:
+            index = index - self._length
+            p = 1.
+
+        transform = transforms.Compose([
+            transforms.RandomHorizontalFlip(p=p),
+            transforms.Resize(self.image_size),
+            transforms.ToTensor()
+        ])
+
+        img_path = self.image_paths[index]
+        image = None
+        try:
+            image = Image.open(img_path)
+        except BaseException as e:
+            print(img_path)
+
+        if not image.mode == 'RGB':
+            image = image.convert('RGB')
+
+        image = transform(image)
+
+        if self.to_normal:
+            image = (image - 0.5) * 2.
+            image.clamp_(-1., 1.)
+
+        height, width = self.image_size
+        mask_width = random.randint(128, 180)
+        mask_height = random.randint(128, 180)
+        mask_pos_x = random.randint(0, height - mask_height)
+        mask_pos_y = random.randint(0, width - mask_width)
+        mask = torch.ones_like(image)
+        mask[:, mask_pos_x:mask_pos_x+mask_height, mask_pos_y:mask_pos_y+mask_width] = 0
+
+        cond_image = image * mask
+
+        image_name = Path(img_path).stem
+        return (image, image_name), (cond_image, image_name)
+
 
 
 def remove_file(fpath):
